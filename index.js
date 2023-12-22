52032e9e97338a0fa7885fd905a7bb4e10a1658a
Saad Srabon <saadsrabon2@gmail.com>
2023-12-22 14:04:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Task Practice the module In mOdule 5 ,
+//Complete the assignment
+//Start Watching MOdule - 6 6 videos -with Practice
+const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const initialState =[]
+
+const fetchPost = createAsyncThunk("post/fetchPost", async () => {
+    // will fetch the data from here
+    // and update it to the next middileware
+});
