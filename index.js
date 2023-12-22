433551ba3a8062b58c94c84a68cafa9c36b9e94d
Saad Srabon <saadsrabon2@gmail.com>
2023-12-22 14:45:41 +0600

    redux thunk use to fetch data

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 // Task Practice the module In mOdule 5 ,
 //Complete the assignment
 //Start Watching MOdule - 6 6 videos -with Practice
-const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const {createAsyncThunk, createSlice, configureStore }= require("@reduxjs/toolkit");
 const initialState =[]
 
 const fetchPost = createAsyncThunk("post/fetchPost", async () => {
-    // will fetch the data from here
-    // and update it to the next middileware
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    return data;
 });
+
+const postReducer = createSlice({
+    name: "post",
+    initialState,
+    extraReducers: builder => {
+        builder.addCase(fetchPost.fulfilled, (state, action) => {
+            return action.payload;
+        });
+    }
+});
+
+const store = configureStore({
+    reducer: {
+        post: postReducer.reducer
+    }
+});
+
+
+const render = () => {
+    console.log(store.getState());
+}
+store.subscribe(render);
+
+store.dispatch(fetchPost());
